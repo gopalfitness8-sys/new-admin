e92f0c70f4f074e0d30e1d27f265b2ac9b2367c1
gopal <gopalfitness8@gmail.com>
2026-04-06 11:31:38 +0530

    admin

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://187.127.134.36:5000/api',
+    baseURL: import.meta.env.VITE_API_URL || 'https://api.mhmatka.space/api',
 });
 
 api.interceptors.request.use(
